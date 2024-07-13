1044427698122e5c4fee3da8903134049123971b
haricypress <pirate.evil.war@gmail.com>
2024-07-13 21:39:04 +0530

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-   baseUrl: 'https://opensource-demo.orangehrmlive.com/',
-
-    env: {
-      username: "Admin",
-      password: "admin123"
-    },
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
